b1db7ea9cd09d9c685f839b30697dc15c76a7b6d
Richie Doherty <dohertyrichie@gmail.com>
2023-06-23 08:51:46 -0400

    Added the ability to add a location on facebook post

--- social-media-marketing-bot/src/Bots/post_bot.py
+++ social-media-marketing-bot/src/Bots/post_bot.py
@@ -8,6 +8,7 @@ sys.path.append('//Users//Mackdig25//rdoherty2019.github.io//social-media-market
 from gsheets import GSheet
 from handle_images import HandleImage
 from my_secrets import max_character_limit
+from route_social_media import SocialMedia
 
 # Social media platforms to target
 social_media_accounts = ['facebook']
@@ -27,3 +28,11 @@ if __name__ == '__main__':
         post_image = HandleImage()
         post_image.getImageFromSource(image_link=gsheet.post_details['post_image_link'])
         post_image.resizeImage(social_media_account=social_media_account)
+
+        # Calling the social media
+        social_media_platform = SocialMedia(social_media_account=social_media_account)
+        social_media_platform.setSocialMediaClass()
+        social_media_platform.composeSocialMediaContent(image_filename=post_image.image_filename,
+                                                        post_details=gsheet.post_details)
+        social_media_platform.postSocialMediaContent()
+        #social_media_platform.addLocationSocialMediaContent()
